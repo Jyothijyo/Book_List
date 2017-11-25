460f988c7dfd861d00c249e8a0d4431345cd7733
Jyothi <jyosowmy@gmail.com>
2017-11-25 13:14:58 +0530

    Book list reducer

--- src/components/app.js
+++ src/components/app.js
@@ -1,9 +1,14 @@
-import React, { Component } from 'react';
+import React from 'react';
+import BookList from '../containers/book-list';
+import BookDetail from '../containers/book-detail';
 
-export default class App extends Component {
+export default class App extends React.Component {
   render() {
     return (
-      <div>React simple starter</div>
+      <div>
+        <BookList />
+        <BookDetail />
+      </div>
     );
   }
 }
---
+++ src/containers/book-list.js
@@ -0,0 +1,43 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { selectBook } from '../actions/index';
+import { bindActionCreators } from 'redux';
+
+class BookList extends Component {
+
+  renderList() {
+    return this.props.books && this.props.books.map((book) => {
+      return (
+        <li
+          key={book.title}
+          className="list-group-item"
+          onClick={() => this.props.selectBook(book)}
+        >
+          {book.title}
+        </li>
+      )
+    })
+  }
+
+  render() {
+    return (
+      <ul className="list-group col-sm-4">
+        {this.renderList()}
+      </ul>
+    )
+  }
+}
+
+function mapStateToProps(state) {
+  //whatever is returned will show up props inside BookList
+  return {
+    books: state.books
+  };
+  //glue b/w react and redux
+}
+
+function mapDispatchToProps(dispatch) {
+  return bindActionCreators({ selectBook: selectBook}, dispatch);
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(BookList);
